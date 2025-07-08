7eed33e853158e28d60d9495f20644e061307b77
letvarprint <a.brixencov@icloud.com>
2025-07-08 06:35:40 +0300

    new interface elements

--- Homework/ViewController.swift
+++ Homework/ViewController.swift
@@ -4,11 +4,18 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let userRepository = UserRepository()
+    private let textLabel = UILabel()
+    private let button: UIButton = {
+       let button = UIButton()
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .green
+        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        view.alpha = 0.9
+        view.backgroundColor = .yellow
         addUsers()
         printNames()
     }
@@ -16,6 +23,9 @@ class ViewController: UIViewController {
     private func addUsers() {
         let userList = userRepository.getUsers()
         helper.addManyUsers(userList)
+        setupLabel()
+        view.addSubview(textLabel)
+        view.addSubview(button)
     }
     
     private func printNames() {
@@ -23,5 +33,14 @@ class ViewController: UIViewController {
         for name in usersFullName {
             print(name)
         }
+    }
+    
+    private func setupLabel() {
+        if let text = helper.getUsers().randomElement()?.personalData.fullName {
+            textLabel.text = text
+        }
+        textLabel.font = .systemFont(ofSize: 25)
+        textLabel.textColor = .blue
+        textLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
     }
 }
